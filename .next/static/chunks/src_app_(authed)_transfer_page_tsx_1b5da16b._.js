8e404e26d4de73c2455c25be74d96c2282543388
CHUO Kimheng <hengk7401@gmail.com>
2025-07-13 15:54:44 +0000

    u

---
+++ .next/static/chunks/src_app_(authed)_transfer_page_tsx_1b5da16b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_313dc86e._.js",
+  "static/chunks/src_99458e77._.js"
+],
+    source: "dynamic"
+});
